f6f5fe1df98e1d4df3b1d77fa3bdc22115059740
andreybannikov <andrewnnov@yandex.ru>
2018-12-24 23:23:10 +0300

    Golovach_ex

---
+++ src/main/java/ru/bannikov/exceptions/App00.java
@@ -0,0 +1,35 @@
+package ru.bannikov.exceptions;
+
+/*
+the power of operators
+continue
+break
+return
+throw
+ */
+
+public class App00 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 2 ; i++) {
+            f();
+            System.err.println();
+        }
+    }
+
+    private static void f() {
+        for (int i = 0; i < 2 ; i++) {
+            System.err.print(0);
+            if(true)
+            //continue;
+            //break;
+            //return;
+            //throw new Error();
+            System.err.print(1);
+        }
+        System.err.print(2);
+    }
+
+
+}
---
+++ src/main/java/ru/bannikov/exceptions/App02.java
@@ -0,0 +1,32 @@
+package ru.bannikov.exceptions;
+
+/*
+
+ */
+
+public class App02 {
+
+    public static void main(String[] args) {
+        System.err.println(">0");
+        f();
+        System.err.println("<0");
+    }
+
+    private static void f() {
+        System.err.println("  >1");
+        g();
+        System.err.println("  <1");
+    }
+
+    private static void g() {
+
+        System.err.println("    >2");
+        h();
+        System.err.println("    <2");
+    }
+
+    private static void h() {
+
+        throw new RuntimeException();
+    }
+}
